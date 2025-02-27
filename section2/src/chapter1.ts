2febe103c8df7f4ff3a7ed0b8751c15e40e4d592
(조연정) <jojoor201@gmail.com>
2025-02-27 20:00:52 +0900

    day2

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,35 @@
+// number
+let num1: number = 123;
+let num2: number = -1;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// String
+let str1: string = "hello";
+let str2: string = "hello";
+let str3: string = `hello`;
+let str4: string = `hello${num1}`;
+
+// boolean
+let boo1: boolean = true;
+let boo2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// let numA: number = null;
+
+// 리터럴 타입
+// 리터럴 -> 값
+let numA: 10 = 10;
+numA = 12;
+
+let strA: "hello" = "hello";
+
+let boolA: true = false;
