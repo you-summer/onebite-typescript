32d6c577a84fa507f843db22c82ae50693b610d4
(조연정) <jojoor201@gmail.com>
2025-03-10 19:04:36 +0900

    day11

---
+++ section7/src/chapter3.ts
@@ -0,0 +1,100 @@
+/**
+ * 제네릭 인터페이스
+ */
+
+interface KeyPair<K, V> {
+  key: K;
+  value: V;
+}
+
+let keyPair: KeyPair<string, number> = {
+  key: "key",
+  value: 0,
+};
+
+// 제네릭 인터페이스를 사용할때는 반드시 타입으로 정의할때 타입변수에 할당할 타입을 꺾쇠와 함께 반드시 사용해야함
+
+// 타입 변수 = 타입 파라미터 = 제네릭 타입변수 = 제네릭 타입 파라미터
+
+let keyPair2: KeyPair<boolean, string[]> = {
+  key: true,
+  value: ["1"],
+};
+
+/**
+ * 인덱스 시그니처
+ */
+interface NumberMap {
+  [key: string]: number;
+}
+
+let numberMap1: NumberMap = {
+  key: -12345,
+  key2: 1234123,
+};
+
+interface Map<V> {
+  [key: string]: V;
+}
+
+let stringMap: Map<string> = {
+  key: "valuec",
+};
+
+let booleanMap: Map<boolean> = {
+  key: true,
+};
+
+/**
+ * 제네릭 타입 별칭
+ */
+
+type Map2<V> = {
+  [key: string]: V;
+};
+
+let stringMap2: Map2<string> = {
+  key: "hello",
+};
+
+/**
+ * 제네릭 인터페이스 활용 예시
+ * -> 유저 관리 프로그램
+ * -> 유저 구분 : 학생 유저 / 개발자 유저
+ */
+
+interface Student {
+  type: "student";
+  school: string;
+}
+
+interface Developer {
+  type: "developer";
+  skill: string;
+}
+
+interface User<T> {
+  name: string;
+  profile: T;
+}
+
+function goToSchool(user: User<Student>) {
+  const school = user.profile.school;
+  console.log(`${school}로 등교 완료`);
+}
+
+const developerUser: User<Developer> = {
+  name: "이정환",
+  profile: {
+    type: "developer",
+    skill: "TypeScript",
+  },
+};
+
+const studentUser: User<Student> = {
+  name: "홍길동",
+  profile: {
+    type: "student",
+    school: "가톨릭대학교",
+  },
+};
